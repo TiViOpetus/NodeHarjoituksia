bee9743af6d0e81566cd43034ef4b135c537f0c4
Mika Vainio <koti.mika.vainio@gmail.com>
2023-09-19 15:53:07 +0300

    Created scripts for exercises in readme.md

---
+++ VuosiKuukausiEdellinenKuukausi.sql
@@ -0,0 +1,5 @@
+-- Vuoden, kuukauden ja edellisen kuukauden erottaminen 
+-- kuluvan ajanhetken aikaleimasta
+SELECT EXTRACT(year FROM NOW()) AS vuosi,
+	EXTRACT(month FROM NOW()) AS kuukausi,
+	EXTRACT(month FROM NOW())-1 AS edellinen_kuukausi
