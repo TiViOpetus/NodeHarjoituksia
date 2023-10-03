8a6cbbf02a0fd7812c4a30e72a3b3331298e7852
Mika Vainio <koti.mika.vainio@gmail.com>
2023-10-03 15:28:19 +0300

    Added some comments to clarify logging

--- microservice.js
+++ microservice.js
@@ -41,6 +41,8 @@ cron.schedule('*/5 15 * * *', () => {
     // If the date of last successful fetch is not the current day, fetch data
     if (lastFetchedDate != dateStr) {
       message = 'Started fetching price data'
+
+      // Log event to  console and log file
       console.log(message);
       logger.add2log(message, logFile)
       getPrices.fetchLatestPriceData().then((json) => {
@@ -50,7 +52,8 @@ cron.schedule('*/5 15 * * *', () => {
           let values = [element.startDate, element.price];
 
           // Build a SQL clauset to insert values into table
-          const sqlClause = 'INSERT INTO public.hourly_price VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING *'; 
+          const sqlClause = 'INSERT INTO public.hourly_price VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING *';
+
           // Function for running SQL operations asyncronously
           const runQuery = async () => {
             let resultset = await pool.query(sqlClause, values);
@@ -58,28 +61,38 @@ cron.schedule('*/5 15 * * *', () => {
           }
           // Call query function and echo results to console
           runQuery().then((resultset) => {
-            if (resultset.rows[0] != undefined ) {
-              message = 'Added a row'
+
+            // If there is alredy a price for thetimeslot, row is empty ie. undefined
+            if (resultset.rows[0] != undefined) {
+              message = 'Added a row' // The message when not undefined
             }
             else {
-              message = 'Skipped an existing row'
+              message = 'Skipped an existing row' // The message when undefined
             }
+
+            // Log the result of insert operation
             console.log(message);
             logger.add2log(message, logFile)
 
           })
-          
+
         });
       });
       lastFetchedDate = dateStr; // Set fetch date to current date
+
+      // Log when last feched
       message = 'Fetched at ' + lastFetchedDate;
       console.log(message)
       logger.add2log(message, logFile)
     } else {
+
+      // Log if data has been retrieved earlier at the same day
       message = 'Next scheduled event: Data has been successfully retrieved earlier today'
       console.log(message);
       logger.add2log(message, logFile)
     }
+
+    // Log an error if something has been failed to run
   } catch (error) {
     message = 'An error occurred (' + error.toString() + '), trying again in 5 minutes until 4 PM';
     console.log(message)
