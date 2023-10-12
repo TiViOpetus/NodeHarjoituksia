4d3c57d291e2b056c6f31343752a160670120988
Mika Vainio <koti.mika.vainio@gmail.com>
2023-10-12 15:03:54 +0300

    Update exercises.js

--- Keratausharjoitus/exercises.js
+++ Keratausharjoitus/exercises.js
@@ -106,14 +106,177 @@ console.log('Assin keskiarvo on', coder4.calculateAverage([2,5,4,5]))
 /* Harjoitus 7 Muuta edellisen harjoituksen funktio ES6-muotoon
 */
 
+const stdDev = (array, decimals) => {
+    const deviation = mathjs.std(array);
+    const roundedDev = mathjs.round(deviation, decimals);
+    return roundedDev;
+}
+
+console.log(stdDev([1,3,1,3], 1))
+
 /* Harjoitus 8 Määrittele luokka, jossa on metodit keskiarvon,
 keskihajonnan ja varianssin, suurimman ja pienimmän arvon laskemiseksi
 argumenttina annetusta vektorista. */
 
-/* HARJOITUS 9
-Tee edellisen tehtävän luokasta CJS kirjasto ja anna se muiden ohjelmien käyttöön export-komennolla
-Luo node.js-ohjelma, jossa luokasta luodaan 5 oliota ja tallennetaan ne vektoriin.
-
+/* Keskimäärin voi tarkoittaa montaa eri tavalla laskettua arvoa:
+1. Keskiarvo (average) on lukujen summa jaettuna niiden lukumäärällä
+2. Keskiluku (mean) luvut järjestetään suuruusjärjestykseen ja otetaan keskimäinen arvo. 
+Jos sitä ei ole, otetaan kahden keskimmäisen luvun keskiarvo
+3. Tyyppiarvo (mode) arvo, joka esiintyy useimmin listassa
 */
 
+class ArrayStats {
+    constructor(array, decimals) {
+        this.array = array;
+        this.decimals = decimals;
+    }
 
+    average() {
+        let avg = 0; // Alustetaan keskiarvo 0:ksi
+        let sum = 0; // Alustetaan summa 0:ksi
+
+        // Käydään alkiot läpi silmukassa
+        for (let index = 0; index < this.array.length; index++) {
+            const element = this.array[index];
+            sum = sum + element
+            
+        }
+
+        // Keskiarvo on lukujen summa jaettuna lukumäärällä
+        avg = sum / this.array.length;
+        return avg; // Palautetaan laskettu keskiarvo
+        } 
+
+    
+    variance() {
+        const arrayVarianve = mathjs.variance(this.array);
+        const roundedArrayVariance = mathjs.round(arrayVarianve, this.decimals);
+        return roundedArrayVariance;
+
+    }
+    stdDev() {
+        const arrayDeviation = mathjs.std(this.array);
+        const roundeArrayDeviation = mathjs.round(arrayDeviation, this.decimals);
+        return roundeArrayDeviation;
+
+    }
+
+    max() {
+        const arrayMax= mathjs.max(this.array);
+        const roundeArrayMax = mathjs.round(arrayMax, this.decimals);
+        return roundeArrayMax;
+
+    }
+
+    min() {
+        const arrayMin= mathjs.min(this.array);
+        const roundeArrayMin = mathjs.round(arrayMin, this.decimals);
+        return roundeArrayMin;
+    }
+
+}
+
+const arrayStats = new ArrayStats([1,3,1,3], 1)
+console.log('Keskiarvo on', arrayStats.average())
+console.log('Varianssi on', arrayStats.variance())
+console.log('Keskihajonta on', arrayStats.stdDev())
+console.log('Minimi on', arrayStats.min())
+console.log('Maksimi on', arrayStats.max())
+
+/* HARJOITUS 9
+Tee edellisen tehtävän luokasta CJS kirjasto ja anna se muiden ohjelmien käyttöön export-komennolla
+*/
+// A CJS LIBRARY TO CALCULATE BASIC STATISTICAL CHARASTERISTICS OF AN ARRAY
+// ========================================================================
+
+// LIBRARIES
+// ---------
+
+// Statistical operations are based on math.js library
+// It is not built-in Math class, need to install this 1st
+const mathjs = require('mathjs'); 
+
+// A class to calculate statistical characteristics at given precision
+class ArrayStats {
+    constructor(array, decimals) {
+        this.array = array;
+        this.decimals = decimals;
+    }
+
+
+    // Average ie aritmeettinen keskiarvo 
+    mean() {
+        const arrayMean = mathjs.mean(this.array);
+        const roundedMean = mathjs.round(arrayMean, this.decimals);
+        return roundedMean
+
+
+    }
+
+    /* Median ie keskiluku eli järjestetyn lukujonon keskimmäinen arvo 
+    (pariton määrä lukuja) tai kahden keskimmäisen arvon keskiarvo 
+    (parillinen määrä lukuja)
+    */
+    median() {
+        const arrayMedian = mathjs.median(this.array);
+        const roudedArrayMedian = mathjs.round(arrayMedian, this.decimals);
+        return roudedArrayMedian;
+
+    }
+    // Mode ie tyyppiarvo eli luku, joka esiintyy eniten lukujoukossa
+    mode() {
+        const arrayMode = mathjs.mode(this.array);
+        return arrayMode
+
+    }
+
+    // Variation of popultation ie populaation varianssi
+    populationVariance() {
+        const arrayVariance = mathjs.variance(this.array, 'uncorrected');
+        const roundedArrayVariance = mathjs.round(arrayVariance, this.decimals);
+        return roundedArrayVariance;
+
+    }
+
+    // Standard deviation of population ie populaation keskihajonta
+    populationStdDev() {
+        const arrayDeviation = mathjs.std(this.array, 'uncorrected');
+        const roundedArrayDeviation = mathjs.round(arrayDeviation, this.decimals);
+        return roundedArrayDeviation;
+
+    }
+
+    // Maximum value of array ie maksimi tai suurin arvo
+    max() {
+        const arrayMax = mathjs.max(this.array);
+        const roundedArrayMax = mathjs.round(arrayMax, this.decimals);
+        return roundedArrayMax;
+
+    }
+
+    // Minimum value of array ie minimi tai pienin arvo
+    min() {
+        const arrayMin = mathjs.min(this.array);
+        const roundedArrayMin = mathjs.round(arrayMin, this.decimals);
+        return roundedArrayMin;
+    }
+
+}
+
+module.exports = {
+    ArrayStats
+}
+
+/*
+TESTATAAN, ETTÄ LUOKKA ON KÄYTETTÄVISSÄ TOISESSA MODUULISSA (suttu.js)
+*/
+
+const stats = require('./statistics');
+
+const arrayStats = new stats.ArrayStats([1,2,3,44,44,22,22,22],1)
+let average = arrayStats.mean() // Average
+let median = arrayStats.median() // Number in the middle of the array
+let mode = arrayStats.mode() // The most common number in the array
+console.log('Ja vektorin alkioiden keskiarvo on', average)
+console.log('Ja vektorin alkioiden keskiluku on', median)
+console.log('Ja vektorin yleisin alkio on', mode)
